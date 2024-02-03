fbffaa0034e12b645d22311632d3c353330dede9
Tomas Lillo Sanhueza <108248835+TommiL90@users.noreply.github.com>
2024-02-03 20:35:09 -0300

    feat: create enviroment

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -22,21 +22,13 @@ const generateDatabaseURL = (schema: string) => {
 export default <Environment>{
   name: 'prisma',
   async setup() {
-    const schema = randomUUID()
-    const databaseURL = generateDatabaseURL(schema)
-
-    process.env.DATABASE_URL = databaseURL
-
-    execSync('npx prisma migrate deploy')
-
+    console.log('Setting up test database...✅')
     return {
-      async teardown() {
-        await prisma.$executeRawUnsafe(
-          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
-        )
-
-        await prisma.$disconnect()
+      teardown() {
+        console.log('Tearing down test database...✅')
       },
     }
   },
+  // Puedes agregar más propiedades según sea necesario
+  transformMode: 'ssr',
 }
